d59c721f100722a4ea41355116b1662a14203828
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-01-26 15:27:02 +0100

    server.js

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express"); /*import express to set up the server*/
+const app = express(); // we assigne our server to the variable `app` in this setup
+const cors = require("cors"); /*import cors to run server with a middleman*/
+
+app.use(cors()); 
+app.use(express.json());
+
+// const resources = [ 
+//     {topic: "fruit", link: ["https://en.wikipedia.org/wiki/Fruit", "link2" ]},
+//     {topic: "meat", link: ["https://en.wikipedia.org/wiki/Meat"]} 
+// ]
+
+const resources = { 
+    fruit: ["https://www.britannica.com/science/fruit-plant-reproductive-body","https://en.wikipedia.org/wiki/Fruit", "https://www.halfyourplate.ca/fruits-and-veggies/fruits-a-z/","https://www.myplate.gov/eat-healthy/fruits","https://www.fruitlogistica.com/en/","https://www.betterhealth.vic.gov.au/health/healthyliving/fruit-and-vegetables"],
+    meat: ["https://en.wikipedia.org/wiki/Meat","https://www.merriam-webster.com/dictionary/meat","https://www.redefinemeat.com/"] 
+}
+
+app.get("/", (req, res) => {
+    res.send('Hello World!');
+  });
+
+app.get("/topics", (req, res) => {
+    res.send(resources);
+  });
+
+app.get("/topics/:id", (req, res) => {
+    try {
+        let quoteId = req.params.id;
+        if (!resources[quoteId]) {
+            throw new Error(`Error: no search results for ${quoteId}.`);
+        }
+        res.send(resources[quoteId]);
+    } catch (err) {
+        res.status(404).send(err.message);        
+    }
+  });
+
+
+
+module.exports = app;
