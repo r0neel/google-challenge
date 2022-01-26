63de2d2871c50adfab67c67b5e9c2a3e3d633ed9
r0neel <ronbhagwakar@gmail.com>
2022-01-26 18:01:56 +0000

    add seperate search button

--- client/script.js
+++ client/script.js
@@ -3,22 +3,25 @@ const searchButton = document.getElementById("searchbutton");
 const randomButton = document.getElementById("randombutton");
 const form = document.querySelector("#theform");
 const resultSpace = document.getElementById("searchresults");
-
 function showResults(e){
     e.preventDefault();
+    let submitter = e.submitter;
+    let handler = submitter.id;
+    if(handler === 'searchbutton'){
     let topic = e.target.sitesearch.value;
     console.log(topic);
     fetch(`http://localhost:3000/topics/${topic}`)
     .then(resp => resp.text())
-    .then(data =>{ resultSpace.textContent =data;
-    //    let results = document.querySelector("results");
-    //    let newItem = document.createElement("p");
-    //    newItem.textContent = data;
-    //    results.appendChild(newItem);
-    })
+    .then(data =>{resultSpace.textContent =data;
+    })}
+    if(handler === 'randombutton'){
+    let topic = e.target.sitesearch.value;
+    console.log(topic);
+    fetch(`http://localhost:3000/topics/${topic}[0]`)
+    .then(resp => resp.text())
+    .then(data =>{resultSpace.textContent =data;
+     })}
 }
-
 //searchButton.addEventListener('click',showResults);
 form.addEventListener('submit',showResults);
 // randomButton.onclick(returnFirst());
-
